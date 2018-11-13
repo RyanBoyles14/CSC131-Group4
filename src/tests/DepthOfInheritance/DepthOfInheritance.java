63ef0dd7e74d7ba78a166c6090f232a6e7384f90
Ryan <ryanboyles@csus.edu>
2018-11-13 14:54:35 -0800

    Began learning and experimenting with ANTLR. Included grammar for Java8 and C++.

---
+++ src/tests/DepthOfInheritance/DepthOfInheritance.java
@@ -0,0 +1,26 @@
+/*
+    Group 4 - CSC 131
+    Developer: Ryan Boyles
+
+    When passing in a group a files, use ANTLR to build a Parse Tree for the all project files
+    For each class, recursively get the class' parent to find the total depth of inheritance
+    Depending on the file extension (in this case, Java or C++), use the necessary ANTLR
+    grammar to parse the file.
+
+    Steps:
+    1) Grab the file(s). Determine if they are Java or C++
+    2) Create a ParseTree using ANTLR. Try to include imports and includes
+    3) Go through each class and compute its depth of inheritance
+    4) Handle results to return
+
+*/
+
+public class DepthOfInheritance {
+    public static void main(String args[]){
+        // https://search.proquest.com/openview/788320e2eb5cf34b858a450075ef6511/1.pdf?pq-origsite=gscholar&cbl=18750&diss=y
+        // A project that used ANTLR for various metrics. Plan on switching over
+
+        // https://stackoverflow.com/questions/2206065/java-parse-java-source-code-extract-methods
+        // JavaParser seems easy to work with, but can't work with C++. Maybe try ANTLR?
+    }
+}
