627d7ca420a6b8ef4c7869e84190e3bf9094df3e
kyleYzn <44690432+kyleYzn@users.noreply.github.com>
2018-11-15 16:16:03 -0800

    test on determine time complexity of the methods(unfinished)

---
+++ src/Complex.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Complex {
+	File f;
+	public Complex(File f) {
+		this.f = f;
+	}
+	
+	public void findMethods(File f) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(f));
+		Boolean isMethod = false;
+		String line;
+		while (br.readLine() != null) {
+			line = br.readLine();
+			if(line.startsWith("public")) {
+				isMethod = true;
+			}
+		}
+	}
+}
