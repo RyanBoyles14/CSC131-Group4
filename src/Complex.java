ad65076c04fe00eb0245ae43431a73347480264f
kyle <kyle1218yang@gmail.com>
2018-11-15 16:37:05 -0800

    store all the method header into the list

--- src/Complex.java
+++ src/Complex.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.List;
 
 public class Complex {
 	File f;
@@ -10,12 +11,13 @@ public class Complex {
 		BufferedReader br = new BufferedReader(new FileReader(f));
 		Boolean isMethod = false;
 		String line;
-		
+		List<String> list = new ArrayList<String>; 
 
 		while (br.readLine() != null) {
 			line = br.readLine();
 			if(line.startsWith("public")) {
 				isMethod = true;
+				list.add(line);
 			}
 		}
 	}
