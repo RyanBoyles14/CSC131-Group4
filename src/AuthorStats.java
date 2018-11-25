85b89f4c2af10b62f1852b2abd418e28dd11ae29
Johnny Au <johnnyau@csus.edu>
2018-11-24 21:16:08 -0800

    builds local .txt of commit messages to parse

--- src/AuthorStats.java
+++ src/AuthorStats.java
@@ -13,51 +13,54 @@ import org.eclipse.jgit.revwalk.RevCommit;
 public class AuthorStats {
 
 	Git git;
-	PrintWriter writer;
 	Iterable<RevCommit> log;
 	Iterator<RevCommit> itr;
 	RevCommit commit;
-	Stack<String> stack;
 	ArrayList<Author> authors;
-	
+
 	// builds a list of Author objects
 	public AuthorStats(Git gitObject) throws NoHeadException, GitAPIException, IOException {
 		git = gitObject;
 		log = git.log().call();
-		buildIDLog();
+		buildLogs();
 
 	}
-	
-	// creates local file "idLog.txt" to parse
-	private void buildIDLog() throws FileNotFoundException, UnsupportedEncodingException {
-		writer = new PrintWriter("idLog.txt", "UTF-8");
+
+	// creates local files to parse
+	private void buildLogs() throws FileNotFoundException, UnsupportedEncodingException {
+		PrintWriter idWriter = new PrintWriter("idLog.txt", "UTF-8");
+		PrintWriter msgWriter = new PrintWriter("msgLog.txt", "UTF-8");
+		Stack<String> idStack = new Stack<>();
+		Stack<String> msgStack = new Stack<>();
 		itr = log.iterator();
-		stack = new Stack<>();
 		while (itr.hasNext()) {
 			commit = itr.next();
-			stack.push(commit.getAuthorIdent().toString());
+			idStack.push(commit.getAuthorIdent().toString());
+			msgStack.push(commit.getShortMessage());
 		}
-		while (!stack.empty()) {
-			writer.println(stack.pop());
+		while (!idStack.empty()) {
+			idWriter.println(idStack.peek());
+			msgWriter.println(idStack.peek());
+			msgWriter.println(msgStack.pop());
+			idStack.pop();
 		}
-		writer.close();
+		idWriter.close();
+		msgWriter.close();
 	}
-	
-	// creates local file "messageLog.txt" to parse
-	private void buildMessageLog() throws FileNotFoundException, UnsupportedEncodingException {}
-	
-	public ArrayList<Author> returnAuthors(){
+
+	public ArrayList<Author> returnAuthors() {
 		return authors;
 	}
 }
 
 // class to store each authors name and commit history
 class Author {
-	
-	String name; 
+
+	String name;
 	int numCommits;
 	ArrayList<String> commitMessages;
-	
-	public Author() {}
-	
+
+	public Author() {
+	}
+
 }
