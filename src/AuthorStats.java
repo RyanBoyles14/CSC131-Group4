42e8b3d91ebf3da90c1b93c98b08ab45e82ee2f8
Johnny Au <johnnyau@csus.edu>
2018-11-24 22:27:34 -0800

    builds list of Author objects

--- src/AuthorStats.java
+++ src/AuthorStats.java
@@ -1,28 +1,51 @@
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Scanner;
 import java.util.Stack;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.NoHeadException;
 import org.eclipse.jgit.revwalk.RevCommit;
 
+// 
 public class AuthorStats {
 
 	Git git;
 	Iterable<RevCommit> log;
 	Iterator<RevCommit> itr;
 	RevCommit commit;
-	ArrayList<Author> authors;
+	ArrayList<Author> authors = new ArrayList<>();
+	HashSet<String> namesList = new HashSet<>();
+	String s, name, email;
 
-	// builds a list of Author objects
+	// builds a list of Author objects from the Git repository
 	public AuthorStats(Git gitObject) throws NoHeadException, GitAPIException, IOException {
 		git = gitObject;
 		log = git.log().call();
 		buildLogs();
+		parseID();
+
+	}
+	
+	// parse "idLog.txt" to create Author objects
+	private void parseID() throws FileNotFoundException {
+		Scanner sc = new Scanner(new File("idLog.txt"));
+		while (sc.hasNextLine()) {
+			s = sc.nextLine();
+			name = s.substring(s.indexOf("[") + 1, s.indexOf(","));
+			email = s.substring(s.indexOf(",") + 2, s.lastIndexOf(","));
+			namesList.add(name);
+			if (!namesList.contains(name)) {
+				authors.add(new Author(name, email));
+			}
+		}
+		sc.close();
 
 	}
 
@@ -64,12 +87,12 @@ class Author {
 		this.name = name;
 		this.email = email;
 	}
-	
+
 	public void add(String msg) {
 		commitMessages.add(msg);
 		numCommits++;
 	}
-	
+
 	public String toString() {
 		String s = String.format("Name: %s\nEmail: %s\n Commits: %d\nHistory:\n", name, email, numCommits);
 		for (String m : commitMessages) {
