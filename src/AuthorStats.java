396ee02453db3b00af7a6524ca10049bf0110ab1
Johnny Au <johnnyau@csus.edu>
2018-11-24 20:28:44 -0800

    comments, separate 'Author' class to store data

--- src/AuthorStats.java
+++ src/AuthorStats.java
@@ -2,6 +2,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Stack;
 import org.eclipse.jgit.api.Git;
@@ -17,14 +18,17 @@ public class AuthorStats {
 	Iterator<RevCommit> itr;
 	RevCommit commit;
 	Stack<String> stack;
-
+	ArrayList<Author> authors;
+	
+	// builds a list of Author objects
 	public AuthorStats(Git gitObject) throws NoHeadException, GitAPIException, IOException {
 		git = gitObject;
 		log = git.log().call();
 		buildIDLog();
 
 	}
-
+	
+	// creates local file "idLog.txt" to parse
 	private void buildIDLog() throws FileNotFoundException, UnsupportedEncodingException {
 		writer = new PrintWriter("idLog.txt", "UTF-8");
 		itr = log.iterator();
@@ -38,4 +42,22 @@ public class AuthorStats {
 		}
 		writer.close();
 	}
+	
+	// creates local file "messageLog.txt" to parse
+	private void buildMessageLog() throws FileNotFoundException, UnsupportedEncodingException {}
+	
+	public ArrayList<Author> returnAuthors(){
+		return authors;
+	}
+}
+
+// class to store each authors name and commit history
+class Author {
+	
+	String name; 
+	int numCommits;
+	ArrayList<String> commitMessages;
+	
+	public Author() {}
+	
 }
