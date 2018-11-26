8065d0ddfdb6696bf22c27ce5ffc5002469ab736
hangrytommy <hangrytommytran@gmail.com>
2018-11-25 16:16:24 -0800

    Add parameter to receive the url to a repository and attempt to open the repository

--- src/Driver.java
+++ src/Driver.java
@@ -1,8 +1,14 @@
+import java.io.IOException;
+import java.nio.file.Files;
+
 import picocli.CommandLine;
 import picocli.CommandLine.*;
 
 public class Driver
 {
+    @Parameters(arity = "1", description = "path to a Github project")
+    String githubProjectPath;
+
     @Option(names = { "-h", "--help" }, description = "print help information")
     boolean printHelp = false;
 
@@ -10,12 +16,30 @@ public class Driver
     {
         Driver app = new Driver();
         CommandLine cmd = new CommandLine(app);
-        cmd.parse(args);
+        try
+        {
+            cmd.parse(args);
+        }
+        catch (MissingParameterException e)
+        {
+            System.err.println("Missing url to repository.");
+            return;
+        }
 
         if (app.printHelp)
         {
             cmd.usage(System.out);
             return;
+        }
+
+        try
+        {
+            Repository repo = new Repository(app.githubProjectPath, Files.createTempDirectory(null).toString());
+            System.out.println("Repository contains " + repo.getFileCount() + " files.");
+        }
+        catch (Exception e)
+        {
+            System.err.println("Could not open repository " + app.githubProjectPath + ": " + e.getMessage());
         }
 
         return;
