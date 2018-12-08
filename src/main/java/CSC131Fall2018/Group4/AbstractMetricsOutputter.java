f57a37f1343e3493bed2f0e9061f6bc61d5610d3
hangrytommy <hangrytommytran@gmail.com>
2018-12-08 15:28:32 -0800

    Allow `AbstractMetricsOutputter.out` to throw `IOException`, `JsonGenerationException`, and `JsonMappingException`

--- src/main/java/CSC131Fall2018/Group4/AbstractMetricsOutputter.java
+++ src/main/java/CSC131Fall2018/Group4/AbstractMetricsOutputter.java
@@ -1,9 +1,13 @@
 package CSC131Fall2018.Group4;
 
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
+import java.io.IOException;
 import java.io.OutputStream;
 
 abstract class AbstractMetricsOutputter
 {
 	abstract public void addMetric(IMetrics metrics);
-	abstract public void out(OutputStream stream);
+	abstract public void out(OutputStream stream)  throws IOException, JsonGenerationException, JsonMappingException;
 }
