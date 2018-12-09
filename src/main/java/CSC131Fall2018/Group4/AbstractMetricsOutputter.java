92c8be510a2784edc2e9c552d6f221a15999fc9c
hangrytommy <hangrytommytran@gmail.com>
2018-12-08 16:11:00 -0800

    Add basic information variables to `AbstractMetricsOutputter`

--- src/main/java/CSC131Fall2018/Group4/AbstractMetricsOutputter.java
+++ src/main/java/CSC131Fall2018/Group4/AbstractMetricsOutputter.java
@@ -8,6 +8,8 @@ import java.io.OutputStream;
 
 abstract class AbstractMetricsOutputter
 {
+	final protected String rootName = "Group4Metrics";
+	final protected double version = 1.0;
 	abstract public void addMetric(IMetrics metrics);
 	abstract public void out(OutputStream stream)  throws IOException, JsonGenerationException, JsonMappingException;
 }
