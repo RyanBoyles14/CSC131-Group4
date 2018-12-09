5a2c30d49bf9e74fb008035c9a25e9f1136c9a53
Johnny Au <johnnyau@csus.edu>
2018-12-09 15:56:21 -0800

    calculation logic moved from Contributor to ContributorMetricsCalculator

--- src/main/java/CSC131Fall2018/Group4/ContributorMetricsCalculator.java
+++ src/main/java/CSC131Fall2018/Group4/ContributorMetricsCalculator.java
@@ -1,6 +1,11 @@
-package CSC131Fall2018.Group4;
+package main.java.CSC131Fall2018.Group4;
 
 import java.io.File;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class ContributorMetricsCalculator extends AbstractMetricsCalculator {
 
@@ -22,11 +27,28 @@ public class ContributorMetricsCalculator extends AbstractMetricsCalculator {
 			c.metrics.name = c.getName();
 			c.metrics.email = c.getEmail();
 			c.metrics.totalCommits = c.getNumCommits();
-			c.metrics.commitPercentage = c.getPercentage();
-			c.metrics.commitsPerMonth = c.getFrequency();
+			c.metrics.commitPercentage = computePercentage(c);
+			c.metrics.commitsPerMonth = computeFreq(c);
 			c.metrics.age = c.getAge();
+		}		
+	}
+	
+	@SuppressWarnings("deprecation")
+	private int computeFreq(Contributor c) {
+		Date initial = new Date(c.initText);
+		Date end = new Date(c.endText);
+		c.days = TimeUnit.DAYS.convert(end.getTime() - initial.getTime(), TimeUnit.MILLISECONDS);
+		LocalDate i = initial.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+		LocalDate e = end.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+		c.diff = Period.between(i, e);
+		if (c.diff.getMonths() == 0) {
+			return c.numCommits;
+		} else {
+			return c.numCommits / (c.diff.getMonths() + 1);
 		}
-			
+	}
+	private double computePercentage(Contributor c) {
+		return (double) c.numCommits / (double) c.total * 100;
 	}
 	
 
