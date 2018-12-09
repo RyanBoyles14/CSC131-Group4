218ab4b2f51f358ac9778a8da719a736ee018098
Johnny Au <johnnyau@csus.edu>
2018-12-09 14:35:57 -0800

    modified to extend AbstractMetricsCalculator

--- src/main/java/CSC131Fall2018/Group4/RepositoryMetricsCalculator.java
+++ src/main/java/CSC131Fall2018/Group4/RepositoryMetricsCalculator.java
@@ -1,9 +1,27 @@
 package main.java.CSC131Fall2018.Group4;
 
-public class RepositoryMetricsCalculator {
+import java.io.File;
 
-	public RepositoryMetricsCalculator(Repository r) {
-		r.metrics.fileCount = r.getList().size();
+public class RepositoryMetricsCalculator extends AbstractMetricsCalculator {
+
+	public RepositoryMetricsCalculator(Repository r) throws Exception {
+		// TODO Auto-generated constructor stub
+		super(r);
 	}
+
+	@Override
+	protected void newCalculation(File f) throws Exception {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	protected void newCalculation(Repository r) throws Exception {
+		// TODO Auto-generated method stub
+		r.metrics.fileCount = r.getList().size();
+		
+	}
+
+	
 
 }
