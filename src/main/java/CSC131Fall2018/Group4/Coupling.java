70a50b9dc204b9f05a6f2069aa2a119314207fae
nathan <nathanwhitfield2014@gmail.com>
2018-12-09 16:17:39 -0800

    Added fix for duplicate interaction entries in Coupling.java.

--- src/main/java/CSC131Fall2018/Group4/Coupling.java
+++ src/main/java/CSC131Fall2018/Group4/Coupling.java
@@ -123,186 +123,214 @@ public class Coupling extends AbstractMetricsCalculator {
 	}
 	
 	//get the component coupling for the classes
-	public void getComponentCoupling() throws IOException{
-		int type;
-		for(int i = 0; i < classes.size(); i++) {
-		BufferedReader buffRead;
-		buffRead = new BufferedReader(new FileReader(fileList.get(classes.get(i).index)));
-		StreamTokenizer st = new StreamTokenizer(buffRead);
-		setTokenizerSyntaxTable(st);
-		st.ordinaryChar(123);
-		st.ordinaryChar(125);
-		st.ordinaryChar(59);
-		st.whitespaceChars(60, 60);
-		st.whitespaceChars(62, 62);
-		st.whitespaceChars(91, 91);
-		st.whitespaceChars(93, 93);
-		boolean classParsed = false;
-		boolean inClass = false;
-		boolean inOtherClass = false;
-		//-1 denotes that we have not yet increased the bracket
-		//number
-		int bracketNumber = -1;
-		int otherClassBracketNumber = -1;
-		String previousToken = null;
-		//evaluates whether the class has finished parsing
-		//or the end of file has been reached
-		do {
-			type = st.nextToken();
-			switch(type) {
+		public void getComponentCoupling() throws IOException{
+			int type;
+			for(int i = 0; i < classes.size(); i++) {
+			BufferedReader buffRead;
+			buffRead = new BufferedReader(new FileReader(fileList.get(classes.get(i).index)));
+			StreamTokenizer st = new StreamTokenizer(buffRead);
+			setTokenizerSyntaxTable(st);
+			st.ordinaryChar(123);
+			st.ordinaryChar(125);
+			st.ordinaryChar(59);
+			st.ordinaryChar(44);
+			st.ordinaryChar(40);
+			st.ordinaryChar(41);
+			st.whitespaceChars(60, 60);
+			st.whitespaceChars(62, 62);
+			st.whitespaceChars(91, 91);
+			st.whitespaceChars(93, 93);
+			boolean classParsed = false;
+			boolean inClass = false;
+			boolean inOtherClass = false;
+			//-1 denotes that we have not yet increased the bracket
+			//number
+			int bracketNumber = -1;
+			int otherClassBracketNumber = -1;
+			String previousToken = null;
 			
-				case StreamTokenizer.TT_WORD:
-					if(previousToken != null && previousToken.equals("class") && st.sval.equals(classes.get(i).classname)) {
-						inClass = true;
-					}
-					else if(previousToken != null && previousToken.equals("class") && !st.sval.equals(classes.get(i).classname) && inClass) {
-						inOtherClass = true;
-					}
-					//here we check for component coupling
-					else if(inClass && !inOtherClass) {
-						for(int j = 0; j < classes.size(); j++) {
-							//if the parsed value is equal to the name of one of
-							//our classes, except the one we are currently in,
-							//then we create a ComponentCoupling entry
-							if(st.sval != null && st.sval.equals(classes.get(j).classname) && !st.sval.equals(classes.get(i).classname)) {
-								st.nextToken();
-								classes.get(i).componentCoupling.add(new ComponentEntry(classes.get(j).classname, st.sval));
-								//need to skip to the semicolon
-								do {
+			//evaluates whether the class has finished parsing
+			//or the end of file has been reached
+			do {
+				type = st.nextToken();
+				//System.out.println(st.sval);
+				switch(type) {
+				
+					case StreamTokenizer.TT_WORD:
+						if(previousToken != null && previousToken.equals("class") && st.sval.equals(classes.get(i).classname)) {
+							inClass = true;
+						}
+						else if(previousToken != null && previousToken.equals("class") && !st.sval.equals(classes.get(i).classname) && inClass) {
+							inOtherClass = true;
+						}
+						//here we check for component coupling
+						else if(inClass && !inOtherClass) {
+							for(int j = 0; j < classes.size(); j++) {
+								//if the parsed value is equal to the name of one of
+								//our classes, except the one we are currently in,
+								//then we create a ComponentCoupling entry
+								if(st.sval != null && st.sval.equals(classes.get(j).classname) && !st.sval.equals(classes.get(i).classname)) {
 									type = st.nextToken();
-								}while((char)st.ttype != ';' && st.ttype != StreamTokenizer.TT_EOF);
+									if(type == StreamTokenizer.TT_WORD) {
+										boolean entryExists = false;
+										for(int k= 0; k < classes.get(i).componentCoupling.size(); k++) {
+											if(classes.get(i).componentCoupling.get(k).targetClassname.equals(classes.get(j).classname) && classes.get(i).componentCoupling.get(k).value.equals(st.sval)) entryExists = true;
+										}
+										if(!entryExists) classes.get(i).componentCoupling.add(new ComponentEntry(classes.get(j).classname, st.sval));
+										//need to skip to the semicolon
+										do {
+											type = st.nextToken();
+										}while((char)st.ttype != ';' && st.ttype != StreamTokenizer.TT_EOF);
+									}
+								}
 							}
 						}
-					}
-					//more to add in
-					previousToken = st.sval;
-					break;
-				//handles whitespace character case
-				//we also use '.' and '(' as whitespaces to evaluate
-				default:
-					//case handling opening brackets as a way to determine
-					//beginning of scope of class
-					if((char)type == '{') {
-						if(!inOtherClass && inClass) {
-							if(bracketNumber == -1) bracketNumber = 1;
-							else bracketNumber++;
-						}
-						else if(inOtherClass && inClass) {
-							if(bracketNumber == -1) otherClassBracketNumber = 1;
-							else otherClassBracketNumber++;
-						}
-					}
-					//case handling closing brackets as a way to determine
-					//end of scope of a class
-					else if((char)type == '}') {
-						if(!inOtherClass && inClass) {
-							bracketNumber--;
-							if(bracketNumber == 0) {
-								classParsed = true;
-								bracketNumber = -1;
-								inClass = false;
+						//more to add in
+						previousToken = st.sval;
+						break;
+					//handles whitespace character case and ordinary character case
+					//we also use '.' and '(' as whitespaces to evaluate
+					default:
+						//case handling opening brackets as a way to determine
+						//beginning of scope of class
+						if((char)type == '{') {
+							if(!inOtherClass && inClass) {
+								if(bracketNumber == -1) bracketNumber = 1;
+								else bracketNumber++;
+							}
+							else if(inOtherClass && inClass) {
+								if(otherClassBracketNumber == -1) otherClassBracketNumber = 1;
+								else otherClassBracketNumber++;
 							}
 						}
-						if(inOtherClass && inClass) {
-							otherClassBracketNumber--;
-							if(otherClassBracketNumber == 0) {
-								otherClassBracketNumber = -1;
-								inOtherClass = false;
+						//case handling closing brackets as a way to determine
+						//end of scope of a class
+						else if((char)type == '}') {
+							if(!inOtherClass && inClass) {
+								bracketNumber--;
+								if(bracketNumber == 0) {
+									classParsed = true;
+									bracketNumber = -1;
+									inClass = false;
+								}
+							}
+							if(inOtherClass && inClass) {
+								otherClassBracketNumber--;
+								if(otherClassBracketNumber == 0) {
+									otherClassBracketNumber = -1;
+									inOtherClass = false;
+								}
 							}
 						}
-					}
-			}
-		}while(!classParsed && st.ttype != StreamTokenizer.TT_EOF);
-		
-	}
-	}
+				}
+			}while(!classParsed && st.ttype != StreamTokenizer.TT_EOF);
+			
+		}
+		}
 	
 	//get the interaction coupling for all classes
-	public void getInteractionCoupling() throws IOException{
-		int type;
-		for(int i = 0; i < classes.size(); i++) {
-		BufferedReader buffRead;
-		buffRead = new BufferedReader(new FileReader(fileList.get(classes.get(i).index)));
-		StreamTokenizer st = new StreamTokenizer(buffRead);
-		setTokenizerSyntaxTable(st);
-		boolean classParsed = false;
-		boolean inClass = false;
-		boolean inOtherClass = false;
-		//-1 denotes that we have not yet increased the bracket
-		//number
-		int bracketNumber = -1;
-		int otherClassBracketNumber = -1;
-		String previousToken = null;
-		String methodName = null;
-		//evaluates whether the class has finished parsing
-		//or the end of file has been reached
-		do {
-			type = st.nextToken();
-			switch(type) {
+		public void getInteractionCoupling() throws IOException{
+			int type;
+			for(int i = 0; i < classes.size(); i++) {
+			BufferedReader buffRead;
+			buffRead = new BufferedReader(new FileReader(fileList.get(classes.get(i).index)));
+			StreamTokenizer st = new StreamTokenizer(buffRead);
+			setTokenizerSyntaxTable(st);
+			st.ordinaryChar(123);
+			st.ordinaryChar(125);
+			st.whitespaceChars(60, 60);
+			st.whitespaceChars(62, 62);
+			st.ordinaryChar(91);
+			st.ordinaryChar(93);
+			st.ordinaryChar(46);
+			boolean classParsed = false;
+			boolean inClass = false;
+			boolean inOtherClass = false;
+			//-1 denotes that we have not yet increased the bracket
+			//number
+			int bracketNumber = -1;
+			int otherClassBracketNumber = -1;
+			String previousToken = null;
+			//evaluates whether the class has finished parsing
+			//or the end of file has been reached
+			do {
+				type = st.nextToken();
+				
+				//debug test code
+				if(st.sval != null) System.out.println(st.sval);
+				else System.out.println((char)type);
+				
+				switch(type) {
+				
+					case StreamTokenizer.TT_WORD:
+						if(previousToken != null && previousToken.equals("class") && st.sval.equals(classes.get(i).classname)) {
+							inClass = true;
+						}
+						else if(previousToken != null && previousToken.equals("class") && !st.sval.equals(classes.get(i).classname) && inClass) {
+							inOtherClass = true;
+						}
+						//more to add in
+						previousToken = st.sval;
+						break;
+					//handles whitespace character case
+					//we also use '.' and '(' as whitespaces to evaluate
+					default:
+						//case handling opening brackets as a way to determine
+						//beginning of scope of class
+						if((char)type == '{') {
+							if(!inOtherClass && inClass) {
+								if(bracketNumber == -1) bracketNumber = 1;
+								else bracketNumber++;
+							}
+							else if(inOtherClass && inClass) {
+								if(otherClassBracketNumber == -1) otherClassBracketNumber = 1;
+								else otherClassBracketNumber++;
+							}
+						}
+						//case handling closing brackets as a way to determine
+						//end of scope of a class
+						else if((char)type == '}') {
+							if(!inOtherClass && inClass) {
+								bracketNumber--;
+								if(bracketNumber == 0) {
+									classParsed = true;
+									bracketNumber = -1;
+									inClass = false;
+								}
+							}
+							if(inOtherClass && inClass) {
+								otherClassBracketNumber--;
+								if(otherClassBracketNumber == 0) {
+									otherClassBracketNumber = -1;
+									inOtherClass = false;
+								}
+							}
+						}
+						//case to evaluate whether there is interaction coupling
+						//and creates an interactionEntry if there is
+						else if ((char)type == '.' && inClass && !inOtherClass) {
+							//iterates across all of the component coupling entries to check if
+							//the previously read token contains the variable name from a component
+							//coupling entry
+							for(int j = 0; j < classes.get(i).componentCoupling.size(); j++) {
+								if(previousToken != null && previousToken.contains(classes.get(i).componentCoupling.get(j).value)) {
+									type = st.nextToken();
+									if(type == StreamTokenizer.TT_WORD) {
+										boolean entryExists = false;
+										//handles duplicate cases
+										for(int k= 0; k < classes.get(i).interactionCoupling.size(); k++) {
+											if(classes.get(i).interactionCoupling.get(k).targetClassname.equals(classes.get(i).componentCoupling.get(j).targetClassname) && classes.get(i).interactionCoupling.get(k).value.equals(st.sval)) entryExists = true;
+										}
+										if(!entryExists) classes.get(i).interactionCoupling.add(new InteractionEntry(classes.get(i).componentCoupling.get(j).targetClassname,st.sval));
+									}
+								}
+							}
+							
+						}
+						
+				}
+			}while(!classParsed && st.ttype != StreamTokenizer.TT_EOF);
 			
-				case StreamTokenizer.TT_WORD:
-					if(previousToken != null && previousToken.equals("class") && st.sval.equals(classes.get(i).classname)) {
-						inClass = true;
-					}
-					else if(previousToken != null && previousToken.equals("class") && !st.sval.equals(classes.get(i).classname) && inClass) {
-						inOtherClass = true;
-					}
-					//more to add in
-					previousToken = st.sval;
-					break;
-				//handles whitespace character case
-				//we also use '.' and '(' as whitespaces to evaluate
-				default:
-					//case handling opening brackets as a way to determine
-					//beginning of scope of class
-					if(type == '{') {
-						if(!inOtherClass && inClass) {
-							if(bracketNumber == -1) bracketNumber = 1;
-							else bracketNumber++;
-						}
-						else if(inOtherClass && inClass) {
-							if(bracketNumber == -1) otherClassBracketNumber = 1;
-							else otherClassBracketNumber++;
-						}
-					}
-					//case handling closing brackets as a way to determine
-					//end of scope of a class
-					else if(type == '}') {
-						if(!inOtherClass && inClass) {
-							bracketNumber--;
-							if(bracketNumber == 0) {
-								classParsed = true;
-								bracketNumber = -1;
-								inClass = false;
-								//once the end of class is reached there's no
-								//more need for parsing
-								return;
-							}
-						}
-						if(inOtherClass && inClass) {
-							otherClassBracketNumber--;
-							if(otherClassBracketNumber == 0) {
-								otherClassBracketNumber = -1;
-								inOtherClass = false;
-							}
-						}
-					}
-					//case to evaluate whether there is interaction coupling
-					//and creates an interactionEntry if there is
-					else if (type == '.' && inClass && !inOtherClass) {
-						type = st.nextToken();
-						if(type == StreamTokenizer.TT_WORD) {
-							methodName = st.sval;
-							type = st.nextToken();
-							if(type == '(') {
-								classes.get(i).interactionCoupling.add(new InteractionEntry(previousToken, methodName + "()"));
-							}
-						}
-					}
-					
-			}
-		}while(!classParsed && st.ttype != StreamTokenizer.TT_EOF);
-		
-	}
-	}
+		}
+		}
 }
