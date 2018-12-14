7a8b7870747dfb3974acc446d57d30fbfda86585
hangrytommy <hangrytommytran@gmail.com>
2018-12-14 10:46:27 -0800

    Rename `Coupling` to `CouplingMetricsCalculator`

--- src/main/java/CSC131Fall2018/Group4/CouplingMetricsCalculator.java
+++ src/main/java/CSC131Fall2018/Group4/CouplingMetricsCalculator.java
@@ -6,7 +6,7 @@ import java.io.StreamTokenizer;
 import java.util.*;
 import java.io.*;
 
-public class Coupling extends AbstractMetricsCalculator {
+public class CouplingMetricsCalculator extends AbstractMetricsCalculator {
 	private ArrayList<File> fileList = new ArrayList<File>();
 	ArrayList<ClassStats> classes = new ArrayList<ClassStats>();
 
@@ -60,7 +60,7 @@ public class Coupling extends AbstractMetricsCalculator {
 	}
 
 	//constructor sets the files to be used for the metric
-	public Coupling(Repository r)
+	public CouplingMetricsCalculator(Repository r)
 			throws Exception {
 		super(r);
 	}
